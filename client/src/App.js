a9cb4fdef7ca3f6877c2562c3c338bb6a8ce0d31
sindhupallavi14 <sindhuramanadula14@gmail.com>
2025-08-14 12:17:15 +0530

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+
 import ChatApp from "./components/ChatApp.jsx";
 
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>
 );
